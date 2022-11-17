34ac7694bd99711bda69427aea86a70b412f8b85
W4we1 <Wawel2004@gmail.com>
2022-11-17 11:06:35 +0100

    .

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <html>
+<head>
+    <title>ZSP Shop</title>
+    <link rel="stylesheet"type="text/css"href="style.css">
+    </head>
+    <body>
+    <form action="login.php"method="post">
+        <h3>Zaloguj się</h3>
+        <?php if (isset($_GET['error'])){?>
+        <p class="error"><?php echo $_GET['error'];?></p>
+        <?php } ?>
+        <label>Nazwa użytkownika</label>
+        <input type="text" name="uname" placeholder="Nazwa użytkownika">
+        <br>
+        <label>Hasło</label>
+        <input type="password"name="password" placeholder="Hasło">
+        <br>
+        <button type
+        </form>
+    </body>
 </html>
