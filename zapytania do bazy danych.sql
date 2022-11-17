2af1df37621401955c1b2012b4a99b8f8cf017f4
W4we1 <Wawel2004@gmail.com>
2022-11-17 09:01:14 +0100

    zapis w trakcje

---
+++ zapytania do bazy danych.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (login, password, is_admin) VALUES ("artur123", "123", "0");
+INSERT INTO users (login, password, is_admin) VALUES ("kamil123", "456", "0");
+INSERT INTO users (login, password, is_admin) VALUES ("maciej123", "789", "0");
+INSERT INTO users (login, password, is_admin) VALUES ("tomasz123", "012", "0");
