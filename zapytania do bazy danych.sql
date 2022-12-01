d64eb6551abe19de6d2215225615a81ad839d4c0
W4we1 <Wawel2004@gmail.com>
2022-12-01 10:32:41 +0100

    xd

--- zapytania do bazy danych.sql
+++ zapytania do bazy danych.sql
@@ -1,3 +1,15 @@
 // lista ofert użytkownika
 
 SELECT offers.name, offers.price FROM offers WHERE offers.user_id="2";
+
+// edycja oferty użytkownika 
+
+UPDATE offers SET offers.name="[nowa_wartość]", offers.description="[nowa_wartość_2]", offers.price="[nowa_wartość_3]" WHERE user_id="1" AND offers.id="1";
+
+//widok zakupów użytkownika
+
+SELECT offers.name FROM offers WHERE offers.id="1";
+SELECT offers.description FROM offers WHERE offers.id="1";
+SELECT offers.price FROM offers WHERE offers.id="1";
+
+
