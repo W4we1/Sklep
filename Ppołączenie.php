422b5c38ad4bc444a6cafa84bc1f32123a9620c4
W4we1 <Wawel2004@gmail.com>
2022-12-15 07:54:18 +0100

    Praca w szkole

--- Ppołączenie.php
+++ Ppołączenie.php
@@ -1,9 +1,9 @@
 <?php
 $host = "localhost";
-$login = "root";
-$password = "";
-$db_name "mydb";
-$con = mysqli_connect($host, $login, $password, $db_name);
+$user = "root";
+$password = '';
+$db_name = "mydb";
+$con = mysqli_connect($host, $user, $password, $db_name);
 if(mysqli_connect_errno()) {
     die("Błąd w połączeniu z MySQL: ".mysqli_connect_error());
 }
